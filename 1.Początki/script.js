3a9a17765abee6a7430cbfb7f4c8ef884500024b
xwisniax96 <52212202+xwisniax96@users.noreply.github.com>
2020-12-09 10:46:50 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+//1
+
+console.log("Przemysław");
+
+//2
+
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//3
+
+console.log("Hello World");
+//console.log(Hello World); // bład, zmienna nie została podana
+// console.log("Hello World); //bład, string nie został zamknięty znakiem "
