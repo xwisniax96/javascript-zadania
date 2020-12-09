a623b0e2f6004e7f36c36f5ab35c790505a57b0a
xwisniax96 <52212202+xwisniax96@users.noreply.github.com>
2020-12-09 11:53:13 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,33 @@
+// 1
+
+/*
+let a = 6;
+let imie = "Przemysław";
+let b = 2 + "dwa";
+let flag = true ;
+let c = null;
+
+console.log(a);
+console.log(imie);
+console.log(b);
+console.log(flag);
+console.log(c);
+*/
+
+// 2
+
+/*
+const a = 2;
+const b = 6;
+let sum = 0;
+sum = a+b;
+console.log(sum);
+*/
+
+// 3
+
+/*
+let d;
+console.log(d);
+*/
+//konsola podaje undefinded, ponieważ nic nie jest przypisane do zmiennej 
